e0626d9ea6e6707c84493ba46748315505a54312
Kersly Miñoza <kerslyminoza@Kerslys-iMac.local>
2025-04-29 13:08:45 +0800

    fix moreworks images

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,9 +1,44 @@
+import tiger from "../assets/images/moreworks/Tiger.jpeg";
+import adhu from "../assets/images/moreworks/adhu.jpg";
+import RadPro from "../assets/images/moreworks/radpro.jpg";
+import Naavya from "../assets/images/moreworks/naavya.jpg";
+import WPG from "../assets/images/moreworks/wpg.png";
+import redhot from "../assets/images/moreworks/redhot.png";
+import Po from "../assets/images/moreworks/positivenest.png";
+import baagh from "../assets/images/moreworks/baagh_red.jpg";
+import palm from "../assets/images/moreworks/palm.jpg";
+import livingguild from "../assets/images/moreworks/livingguild.png";
+import danna from "../assets/images/moreworks/danna.jpg";
+import phenom from "../assets/images/moreworks/phenom.png";
+import companion from "../assets/images/moreworks/companion.png";
+import meworking from "../assets/images/moreworks/workin.webp";
+import creativechimp from "../assets/images/moreworks/creativechimp.png";
+import riley from "../assets/images/moreworks/riley.png";
+import tagaysession from "../assets/images/moreworks/tagaysession.jpg";
+import boldmade from "../assets/images/moreworks/boldmade.png";
+import hm from "../assets/images/moreworks/hm.jpg";
+import nodies from "../assets/images/moreworks/nodies_logo.png";
+import lover from "../assets/images/moreworks/lover.jpg";
+import freytech from "../assets/images/moreworks/freytech.png";
+import yanny from "../assets/images/moreworks/yanny.png";
+import anros from "../assets/images/moreworks/anros_logo.png";
+import omala from "../assets/images/moreworks/omala.jpg";
+import ohna from "../assets/images/moreworks/ohna.png";
+import mybabies from "../assets/images/moreworks/dawgs.jpg";
+import selfportrait from "../assets/images/moreworks/kersly.jpg";
+import orcaa from "../assets/images/moreworks/orcaa_logo.png";
+import badianwellness from "../assets/images/moreworks/badjian.jpg";
+import rhcpunlimited from "../assets/images/moreworks/rhcp.jpeg";
+import beagles from "../assets/images/moreworks/beagles.jpg";
+import riseabovefest from "../assets/images/moreworks/riseabove.jpg";
+import mishka from "../assets/images/moreworks/green.jpg";
+
 export const WORKS = [
     {
       id: 0,
       name: 'Tiger',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/Tiger.jpeg",
+      image: tiger,
     },
 
 
@@ -11,28 +46,28 @@ export const WORKS = [
       id: 1,
       name: 'Adhu',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/adhu.jpg",
+      image: adhu,
     },
 
     {
       id: 1,
       name: 'RadPro Logo',
       type: 'Logo',
-      image: "/src/assets/images/moreworks/radpro.jpg",
+      image: RadPro,
     },
 
     {
       id: 1,
       name: 'Naavya',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/naavya.jpg",
+      image: Naavya,
     },
 
     {
       id: 1,
       name: 'Wordpress Gorilla',
       type: 'Logo',
-      image: "/src/assets/images/moreworks/wpg.png",
+      image: WPG,
     },
 
 
@@ -41,7 +76,7 @@ export const WORKS = [
       id: 1,
       name: 'Red Hot Chili',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/redhot.png",
+      image: redhot,
     },
 
 
@@ -51,63 +86,63 @@ export const WORKS = [
       id: 1,
       name: 'Positive Nest',
       type: 'Logo',
-      image: "/src/assets/images/moreworks/positivenest.png",
+      image: Po,
     },
 
     {
       id: 1,
       name: 'The Palm Tree',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/palm.jpg",
+      image: palm,
     },
 
     {
       id: 1,
       name: 'Living Guild',
       type: 'Logo',
-      image: "/src/assets/images/moreworks/livingguild.png",
+      image: livingguild,
     },
 
     {
       id: 1,
       name: 'Danna',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/danna.jpg",
+      image: danna,
     },
 
     {
       id: 1,
       name: 'Phenom',
       type: 'logo',
-      image: "/src/assets/images/moreworks/phenom.png",
+      image: phenom,
     },
 
     {
       id: 1,
       name: 'Companion',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/companion.png",
+      image: companion,
     },
 
     {
       id: 1,
       name: 'Me Working',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/workin.webp",
+      image: meworking,
     },
 
     {
       id: 1,
       name: 'Creative Chimp',
       type: 'logo',
-      image: "/src/assets/images/moreworks/creativechimp.png",
+      image: creativechimp,
     },
 
     {
       id: 1,
       name: 'Riley',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/riley.png",
+      image: riley,
     },
 
 
@@ -119,21 +154,21 @@ export const WORKS = [
       id: 1,
       name: 'Tagay Session',
       type: 'Poster',
-      image: "/src/assets/images/moreworks/tagaysession.jpg",
+      image: tagaysession,
     },
 
     {
       id: 1,
       name: 'Bold Made',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/boldmade.png",
+      image: boldmade,
     },
 
     {
       id: 1,
       name: 'Baagh',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/baagh_red.jpg",
+      image: baagh,
     },
 
    
@@ -141,7 +176,7 @@ export const WORKS = [
       id: 1,
       name: 'Hm',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/hm.jpg",
+      image: hm,
     },
 
 
@@ -149,28 +184,28 @@ export const WORKS = [
       id: 1,
       name: 'Nodies',
       type: 'logo',
-      image: "/src/assets/images/moreworks/nodies_logo.png",
+      image: nodies,
     },
 
     {
       id: 1,
       name: 'Lover',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/lover.jpg",
+      image: lover,
     },
 
     {
       id: 1,
       name: 'Freytech Website',
       type: 'Web Design',
-      image: "/src/assets/images/moreworks/freytech.png",
+      image: freytech,
     },
 
     {
       id: 1,
       name: 'Yanny',
       type: 'Illustration',
-      image: "/src/assets/images/moreworks/yanny.png",
+      image: yanny,
     },
 
  
@@ -178,28 +213,28 @@ export const WORKS = [
       id: 1,
       name: 'Anros Group',
       type: 'logo',
-      image: "/src/assets/images/moreworks/anros_logo.png",
+      image: anros,
     },
 
     {
       id: 1,
       name: 'Omala',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/omala.jpg",
+      image: omala,
     },
 
     {
       id: 1,
       name: 'Ohna Cakes & Bakes',
       type: 'Logo',
-      image: "/src/assets/images/moreworks/ohna.png",
+      image: ohna,
     },
 
     {
       id: 1,
       name: 'My babies',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/dawgs.jpg",
+      image: mybabies,
     },
 
 
@@ -207,21 +242,21 @@ export const WORKS = [
       id: 1,
       name: 'Self Portrait',
       type: 'Illustrations',
-      image: "/src/assets/images/moreworks/kersly.jpg",
+      image: selfportrait,
     },
 
     {
       id: 1,
       name: 'Orcaa',
       type: 'logo',
-      image: "/src/assets/images/moreworks/orcaa_logo.png",
+      image: orcaa,
     },
 
     {
       id: 1,
       name: 'Badian Wellness',
       type: 'Acrylic Paiting',
-      image: "/src/assets/images/moreworks/badjian.jpg",
+      image: badianwellness,
     },
 
 
@@ -229,7 +264,7 @@ export const WORKS = [
       id: 1,
       name: 'RHCP Unlimited Love',
       type: 'Acrylic Paiting',
-      image: "/src/assets/images/moreworks/rhcp.jpeg",
+      image: rhcpunlimited,
     },
 
 
@@ -239,7 +274,7 @@ export const WORKS = [
       id: 1,
       name: 'Beagles',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/beagles.jpg",
+      image: beagles,
     },
 
 
@@ -248,14 +283,14 @@ export const WORKS = [
       id: 1,
       name: 'Rise Above Fest',
       type: 'Poster',
-      image: "/src/assets/images/moreworks/riseabove.jpg",
+      image: riseabovefest,
     },
 
     {
       id: 1,
       name: 'Mishka',
       type: 'Acrylic Painting',
-      image: "/src/assets/images/moreworks/green.jpg",
+      image: mishka,
     },
 
 
