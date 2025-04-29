8dc0aa2089972c5623f40e47dff72548c423953d
Kersly Miñoza <kerslyminoza@Kerslys-iMac.local>
2025-04-29 12:29:23 +0800

    image url changes

--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -1,8 +1,18 @@
+import orcaa_preview from "../../assets/images/orcaa_thumbnail.png";
+import ironwit_preview from "../../assets/images/ironwit_thumbnail.png";
+import waggle_preview from "../../assets/images/waggle_thumbnail.png";
+import mednotes_preview from "../../assets/images/mednotes_thumbnail.png";
+import valhalla_preview from "../../assets/images/valhalla_thumbnail.png";
+import trux_preview from "../../assets/images/trux_thumbnail.png";
+import poktfundwallet_preview from "../../assets/images/poktfund_thumbnail.png";
+import avatar_preview from "../../assets/images/avatar_thumbnail.gif";
+import hippomathics_preview from "../../assets/images/hippomathics_thumbnail.png";
+
 export const PROJECT = [
   {
     id: 0,
     title: 'ORCAA',
-    image: "/src/assets/images/orcaa_thumbnail.png",
+    image: orcaa_preview,
     description: 'A streamlined app for personalized care management, fostering community engagement and tailored support.',
     path: '/orcaa'
    
@@ -10,7 +20,7 @@ export const PROJECT = [
   {
     id: 1,
     title: 'Ironwit',
-    image: "/src/assets/images/ironwit_thumbnail.png",
+    image: ironwit_preview,
     description:
       'Strength-focused fitness app for advanced gym users, combining workout and diet tracking to deliver more accurate, personalized results.',
     path: '/ironwit'
@@ -18,7 +28,7 @@ export const PROJECT = [
   {
     id: 2,
     title: 'Waggle',
-    image: "/src/assets/images/waggle_thumbnail .png",
+    image: waggle_preview,
     description:
       'All-in-one pet care app that lets you track vaccinations, locate your pet, discover nearby clinics and grooming services, and connect with fellow pet owners.',
     path: '/waggle'
@@ -27,7 +37,7 @@ export const PROJECT = [
   {
     id: 3,
     title: 'Mednotes',
-    image: "/src/assets/images/mednotes_thumbnail.png",
+    image: mednotes_preview,
     description:
       'AI transcription app for doctors that records consultations and instantly generates patient notes or letters.',
     path: '/mednotes'
@@ -35,7 +45,7 @@ export const PROJECT = [
   {
     id: 4,
     title: 'Valhalla',
-    image: "/src/assets/images/valhalla_thumbnail.png",
+    image: valhalla_preview,
     description:
       'Landing page design for a company connecting with top software engineers and IT professionals, featuring a clean, simple, branded look.',
     path: '/valhalla'
@@ -44,26 +54,17 @@ export const PROJECT = [
   {
     id: 5,
     title: 'Trux',
-    image: "/src/assets/images/trux_thumbnail.png",
+    image: trux_preview,
     description:
       'A trading and logistics company specializing in container delivery seeks an in-house system for real-time truck tracking and easy access to transportation history.',
     path: '/trux'
-  },
-
-  {
-    id: 6,
-    title: 'Nodies',
-    image: "/src/assets/images/poktfund_thumbnail.png",
-    description:
-      'A community-driven mobile wallet for seamless, user-friendly access to your POKT and on-chain data—POKT in your pocket, anytime.',
-    path: '/poktfund'
   },
 
 
  {
     id: 7,
     title: 'Poktfund Wallet',
-    image: "/src/assets/images/poktfund_thumbnail.png",
+    image: poktfundwallet_preview,
     description:
       'A community-driven mobile wallet for seamless, user-friendly access to your POKT and on-chain data—POKT in your pocket, anytime.',
     path: '/poktfund'
@@ -74,7 +75,7 @@ export const PROJECT = [
   {
     id: 8,
     title: 'Poktfund Random Avatar',
-    image: "/src/assets/images/avatar_thumbnail.gif",
+    image: avatar_preview,
     description:
       'A randomly generated NFT avatar for the POKT wallet.',
     path: '/avatar'
@@ -84,7 +85,7 @@ export const PROJECT = [
   {
     id: 9,
     title: 'Hippomathics',
-    image: "/src/assets/images/hippomathics_thumbnail.png",
+    image: hippomathics_preview,
     description:
       'A fun Unity 3D project I created for kids, featuring simple and customizable math operations.',
     path: '/hippomathics'
