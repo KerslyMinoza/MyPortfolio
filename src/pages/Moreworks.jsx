1fd726b4e627761ccabac5ea36383c7e3a4bfff8
Kersly <kersly@Kerslys-MacBook-Pro.local>
2025-11-10 00:32:30 +0800

    all fotter

--- src/pages/Moreworks.jsx
+++ src/pages/Moreworks.jsx
@@ -1,5 +1,5 @@
 
-import Footer from '../components/layout/Footer/Footer.jsx';
+import Footer from '../components/layout/components/layout/footer/Footer';
 import Nav from '../components/layout/Nav/Nav.jsx';
 import './moreworks.css';
 import { WORKS } from '../data/works.js';
